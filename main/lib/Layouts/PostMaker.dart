14642ca8e54e34cf5ff7011a9adc2ffa753726ff
salah <boksaloh@gmail.com>
2021-05-07 14:30:54 +0000

    Hello

--- main/lib/Layouts/PostMaker.dart
+++ main/lib/Layouts/PostMaker.dart
@@ -5,64 +5,62 @@ class PostLayout extends StatelessWidget {
   final Post post;
 
   const PostLayout({Key key, this.post}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 400,
-      width: 200,
-      child: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(10),
-            child: Row(
+    return InkWell(
+      borderRadius: BorderRadius.circular(15),
+      onTap: () {},
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        child: Column(
+          children: [
+            Stack(
+              alignment: Alignment.bottomLeft,
               children: [
-                CircleAvatar(
-                  backgroundImage: NetworkImage(post.creator.imageUrl),
-                  radius: 30,
-                ),
-                SizedBox(
-                  width: 5,
-                ),
+                ClipRRect(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                        topRight: Radius.circular(15)),
+                    child: post.post),
                 Container(
-                  child: Text(
-                    post.creator.name,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
+                  padding: EdgeInsets.all(5),
+                  margin: EdgeInsets.all(10),
+                  color: Colors.black87,
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                          backgroundImage: NetworkImage(
+                        post.creator.imageUrl,
+                      )),
+                      Text(
+                        post.creator.name,
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                    ],
                   ),
-                ),
+                )
               ],
             ),
-          ),
-          Container( child: post.post),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("${post.likes} like"),
-            ],
-          ),
-          Row(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  IconButton(
-                      icon: Icon(Icons.favorite_border), onPressed: () {})
-                ],
-              ),
-              SizedBox(
-                width: 250,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  IconButton(icon: Icon(Icons.comment), onPressed: (){})
-                ],
-              )
-            ],
-          )
-        ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Icon(Icons.favorite),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(10, 5, 50, 5),
+                  child: Icon(Icons.comment),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(50, 5, 50, 5),
+                  child: Icon(Icons.share),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
