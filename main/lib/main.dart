446278311f1d4e0b12c0e49fd82911dd242fcd58
salah <boksaloh@gmail.com>
2021-05-07 16:31:37 +0000

    Hello

--- main/lib/main.dart
+++ main/lib/main.dart
@@ -5,7 +5,14 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool typing = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,6 +26,15 @@ class MyApp extends StatelessWidget {
         length: 1,
         child: Scaffold(
           appBar: AppBar(
+            title: typing ? TextBox() : Text("Fac"),
+            leading: IconButton(
+              icon: Icon(typing ? Icons.search : Icons.search),
+              onPressed: () {
+                setState(() {
+                  typing = !typing;
+                });
+              },
+            ),
             bottom: TabBar(
               isScrollable: true,
               tabs: [
@@ -34,6 +50,26 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ) // home: ,
+    );
+  }
+}
+class TextBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(12.0)),
+        color: Colors.white,
+
+      ),
+      child: TextField(
+        decoration:
+        InputDecoration(border: InputBorder.none, hintText: 'Search', enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(12.0)),
+    ),
+    ),
+      ),
     );
   }
 }
